0874b6a481fdcd30c4a7b76536f35271fd2b7ab9
Ali Elsebaie <alielsebaie@Alis-MacBook-Air.local>
2026-05-31 23:36:35 -0400

    update

--- app.py
+++ app.py
@@ -283,10 +283,11 @@ st.markdown("""
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     [data-testid="stToolbar"] {visibility: hidden;}
-    [class^="_profileContainer"] {display: none !important;}
-    [class^="_link_gzau3"] {display: none !important;}
-    [class*="profileContainer"] {display: none !important;}
-    [class*="stBottom"] {display: none !important;}
+    [data-testid="stSidebarNav"] {display: block !important;}
+    section[data-testid="stSidebar"] {
+        min-width: 250px !important;
+        width: 250px !important;
+    }
 </style>
 """, unsafe_allow_html=True)
 
